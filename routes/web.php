15fd36974dd6e4e47f190a86f71b46eb29e3beba
Pedro Sá Ferreira <pedro.saferreira1994@gmail.com>
2024-10-22 18:23:55 +0100

    Adicionei api stripe

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/success', function () {
+    return 'Pagamento realizado com sucesso!';
+});
+
+Route::get('/cancel', function () {
+    return 'O pagamento foi cancelado.';
 });
