5aae9302a81df03178fa2f442476824d7ee7f6c5
Rafael Rodrigues <rafael.rodrigues.t0127544@edu.atec.pt>
2024-10-24 23:02:15 +0100

    Roles added when regesting

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use App\Models\User;
+use App\Models\Role;
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
@@ -43,6 +44,8 @@ class RegisteredUserController extends Controller
             'password' => Hash::make($request->password),
             'birthdate' => $request->birthdate,
         ]);
+        
+        $user->roles()->attach(3);
 
         event(new Registered($user));
 
