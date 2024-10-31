cc06f64c64e8cf5cf75142a5b646aa31097a8daf
GilCosta <gilberto.costa.t0127537@edu.atec.pt>
2024-10-31 23:20:02 +0000

    Carousel added

--- resources/views/components/events/event-public-list.blade.php
+++ resources/views/components/events/event-public-list.blade.php
@@ -1,13 +1,44 @@
-<div class="carousel">
-    <button class="arrow left-arrow">&lt;</button>
-    <div class="carousel-container">
-        <div class="card" style="background-image: url('{{ asset('images/eventoPublicoNosAlive.jpg') }}');"></div>
-        <div class="card" style="background-image: url('{{ asset('images/eventoPublicoNosAlive.jpg') }}');"></div>
-        <div class="card" style="background-image: url('{{ asset('images/eventoPublicoNosAlive.jpg') }}');"></div>
-        <div class="card" style="background-image: url('{{ asset('images/eventoPublicoNosAlive.jpg') }}');"></div>
-        <div class="card" style="background-image: url('{{ asset('images/eventoPublicoNosAlive.jpg') }}');"></div>
+<div class="container swiper">
+    <div class="card-wrapper">
+        <ul class="card-list swiper-wrapper">
+            <li class="card-item swiper-slide">
+                    <a href="#" class="card-link">
+                    <img src="{{ asset('images/Corroios.jpg') }}" alt="Evento" class="card-image">
+                </a>
+            </li>
+            <li class="card-item swiper-slide">
+                <a href="#" class="card-link">
+                    <img src="{{ asset('images/Rock.jpg') }}" alt="Evento" class="card-image">
+                </a>
+            </li>
+            <li class="card-item swiper-slide">
+                <a href="#" class="card-link">
+                <img src="{{ asset('images/RockF.jpg') }}" alt="Evento" class="card-image">
+            </a>
+            </li>
+            <li class="card-item swiper-slide">
+                <a href="#" class="card-link">
+                    <img src="{{ asset('images/RockMusic.jpg') }}" alt="Evento" class="card-image">
+                </a>
+            </li>
+            <li class="card-item swiper-slide">
+                <a href="#" class="card-link">
+                    <img src="{{ asset('images/RockP.jfif') }}" alt="Evento" class="card-image">
+                </a>
+            </li>
+            <li class="card-item swiper-slide">
+                <a href="#" class="card-link">
+                    <img src="{{ asset('images/RockLorem.jpg') }}" alt="Evento" class="card-image">
+                </a>
+            </li>
+            <li class="card-item swiper-slide">
+                <a href="#" class="card-link">
+                    <img src="{{ asset('images/RockI.jfif') }}" alt="Evento" class="card-image">
+                </a>
+            </li>
+        </ul>
+        <div class="swiper-pagination"></div>
+        <div class="swiper-slide-button swiper-button-prev"></div>
+        <div class="swiper-slide-button swiper-button-next"></div>
     </div>
-    <button class="arrow right-arrow">&gt;</button>
 </div>
-
-<!-- VER https://www.youtube.com/watch?v=EtOpz56FyFY -->
--- resources/views/master/main.blade.php
+++ resources/views/master/main.blade.php
@@ -6,7 +6,8 @@
     <title>@yield('title', 'Prime Time Events')</title>
     @vite('resources/css/app.css')
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
-    <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0&icon_names=arrow_forward" />
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css"/>
 </head>
 <body>
     <div class="wrapper">
@@ -22,5 +23,6 @@
         
     </div>
     @vite('resources/js/app.js')
+    <script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
 </body>
 </html>
