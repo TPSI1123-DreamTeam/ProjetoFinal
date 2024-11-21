0cf64df878a0ee6938d1f2a960e5e69d8111dc58
eliezerangel <eliezerangel@gmail.com>
2024-11-21 01:08:41 +0000

    Added Supplier Type

--- resources/views/pages/events/create.blade.php
+++ resources/views/pages/events/create.blade.php
@@ -1,5 +1,5 @@
 @extends('master.main')
 @section('content')
-    @component('components.events.event-create', ['category' => $category, 'suppliers' => $suppliers, 'categories'=> $categories])
+    @component('components.events.event-create', ['category' => $category, 'suppliers' => $suppliers, 'categories'=> $categories, 'SupplierType' => $SupplierType])
     @endcomponent
 @endsection
