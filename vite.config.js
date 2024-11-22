6e12d0d9bd9469d6e43e3cd2fe03157c3dafab24
GilCosta <gilberto.costa.t0127537@edu.atec.pt>
2024-11-22 20:06:59 +0000

    Vite config changes for production

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,21 @@ import laravel from 'laravel-vite-plugin';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: [
+                'resources/css/welcome.css',
+                'resources/css/welcomeFooter.css',
+                'resources/css/welcomeHeader.css',
+                'resources/css/dashboard.css',
+                'resources/css/login.css',
+                'resources/css/register.css',
+                'resources/css/app.css', 
+                'resources/js/app.js'
+            ],
             refresh: true,
         }),
     ],
+    build: {
+        manifest: true,
+        outDir: 'public/build',
+    },
 });
