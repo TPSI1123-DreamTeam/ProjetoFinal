2f4c39ac30caf758735a2cc46d24c73e263b7e27
Pedro Nuno Sá Ferreira <150871417+pedroferr94@users.noreply.github.com>
2024-11-22 18:40:47 +0000

    admin dashboard with users and events crud (#70)
    Co-authored-by: Pedro Sá Ferreira <pedro.saferreira1994@gmail.com>

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -167,7 +167,7 @@ class EventController extends Controller
      */
     public function show(Event $event)
     {
-        //
+        return view('pages.events.show', ['event' => $event]);
     }
 
     /**
@@ -175,7 +175,7 @@ class EventController extends Controller
      */
     public function edit(Event $event)
     {
-        //
+        return view('pages.events.edit', ['event' => $event]);
     }
 
     /**
@@ -183,7 +183,7 @@ class EventController extends Controller
      */
     public function update(UpdateEventRequest $request, Event $event)
     {
-        //
+
     }
 
     /**
@@ -214,5 +214,12 @@ class EventController extends Controller
 
         return view('pages.events.participant', ['events' => $events]);
     }
+
+    public function admin()
+    {
+        $events = Event::all();
+        return view('pages.events.admin', ['events' => $events]);
+    }
+
 
 }
