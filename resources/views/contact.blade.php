c03ecf4be065eddb97a06f166d0d6c3f8aa52839
GilCosta <gilberto.costa.t0127537@edu.atec.pt>
2024-11-25 23:07:26 +0000

    quick fix

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -4,9 +4,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>@yield('title', 'Prime Time Events')</title>
-    @vite('resources/css/welcome.css')
-    @vite('resources/css/header.css')
-    @vite('resources/css/footer.css')
+    @vite('resources/css/client-side/welcome.css')
+    @vite('resources/css/client-side/header.css')
+    @vite('resources/css/client-side/footer.css')
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
     <link href="https://fonts.googleapis.com/css?family=Ubuntu+Mono:400,700" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/gh/yesiamrocks/cssanimation.io@1.0.3/cssanimation.min.css" rel="stylesheet">
