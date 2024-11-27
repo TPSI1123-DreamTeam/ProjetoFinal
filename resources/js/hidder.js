413a53c5dc562689df0163a6a58c25eff621518c
Gilberto Costa <158103790+GilCosta97@users.noreply.github.com>
2024-11-27 21:03:54 +0000

    Event list animation added, sidebar options fixed, header css finalized (except responsiveness) (#88)

---
+++ resources/js/hidder.js
@@ -0,0 +1,4 @@
+document.getElementById('hidder').addEventListener('click', function() {
+    var p = document.getElementById('hidden');
+    p.style.display = (p.style.display === 'none') ? 'block' : 'none';
+});
--- resources/views/components/events/manager/event-list.blade.php
+++ resources/views/components/events/manager/event-list.blade.php
@@ -2,8 +2,11 @@
 <link href="https://cdn.jsdelivr.net/npm/flowbite@2.5.2/dist/flowbite.min.css"  rel="stylesheet" />
 
 <div class="event-wrapper">
-    <h1>Lista de Eventos</h1>
-        <p>Bem-vindo à página de gestão de eventos! 
+    <div class="title-hidder-div">
+        <h1>Lista de Eventos</h1>
+        <button type="button" id="hidder"><i class='bx bx-help-circle bx-tada' ></i></button>
+    </div>
+        <p id="hidden" style="">Bem-vindo à página de gestão de eventos! <br>
             Aqui pode visualizar todos os eventos disponíveis no nosso site, 
             com acesso rápido a informações detalhadas como o nome do evento, data, local e estado. 
             Esta ferramenta permite-lhe gerir os eventos de forma eficiente, 
@@ -29,14 +32,16 @@
         <!-- Campo 2 e 3-->
         <div class="flex items-center space-x-2">
             <label for="participantes" class="text-sm font-medium text-gray-700">Participantes</label>
-
-            <input type="text" name="participants1" id="participants1" 
-            value="{{ isset($formFields['participants1']) ? $formFields['participants1'] : '' }}"
-            class="block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm" placeholder="De" oninput="this.value = this.value.replace(/[^0-9]/g, '');">
+            <div class="event-input-list">
+                <input type="text" name="participants1" id="participants1" 
+                value="{{ isset($formFields['participants1']) ? $formFields['participants1'] : '' }}"
+                class="block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm" placeholder="De" oninput="this.value = this.value.replace(/[^0-9]/g, '');">
+                
+                <input type="text" name="participants2" id="participants2" 
+                value="{{ isset($formFields['participants2']) ? $formFields['participants2'] : '' }}"
+                class="block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm" placeholder="Até" oninput="this.value = this.value.replace(/[^0-9]/g, '');">
+            </div>
             
-            <input type="text" name="participants2" id="participants2" 
-            value="{{ isset($formFields['participants2']) ? $formFields['participants2'] : '' }}"
-            class="block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm" placeholder="Até" oninput="this.value = this.value.replace(/[^0-9]/g, '');">
         </div>
 
         <!-- Campo 4 -->
@@ -55,14 +60,15 @@
         <!-- Campo 2 e 3-->
         <div class="flex items-center space-x-2">
             <label for="participantes" class="text-sm font-medium text-gray-700">Custo</label>
-
-            <input type="text" name="amount1" id="amount1" 
-            value="{{ isset($formFields['amount1']) ? $formFields['amount1'] : '' }}"
-            class="block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm" placeholder="De" oninput="this.value = this.value.replace(/[^0-9]/g, '');">
-            
-            <input type="text" name="amount2" id="amount2" 
-            value="{{ isset($formFields['amount2']) ? $formFields['amount2'] : '' }}"
-            class="block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm" placeholder="Até" oninput="this.value = this.value.replace(/[^0-9]/g, '');">
+            <div class="event-input-list">
+                <input type="text" name="amount1" id="amount1" 
+                value="{{ isset($formFields['amount1']) ? $formFields['amount1'] : '' }}"
+                class="block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm" placeholder="De" oninput="this.value = this.value.replace(/[^0-9]/g, '');">
+                
+                <input type="text" name="amount2" id="amount2" 
+                value="{{ isset($formFields['amount2']) ? $formFields['amount2'] : '' }}"
+                class="block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm" placeholder="Até" oninput="this.value = this.value.replace(/[^0-9]/g, '');">
+            </div>
         </div>
 
         <div class="flex items-center space-x-2">
@@ -79,11 +85,13 @@
 
         <!-- Campo 5 -->
         <div class="flex items-center space-x-2">
-            <label for="campo5" class="text-sm font-medium text-gray-700">Data</label>   
-            <input id="datepicker1" name="datepicker1" datepicker datepicker-format="yyyy-mm-dd" value="{{ isset($formFields['datepicker1']) ? $formFields['datepicker1'] : '' }}" type="text" 
-            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Selecionar Data">
-            <input id="datepicker2" name="datepicker2" datepicker datepicker-format="yyyy-mm-dd" value="{{ isset($formFields['datepicker2']) ? $formFields['datepicker2'] : '' }}" type="text" 
-            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Selecionar Data">
+            <label for="campo5" class="text-sm font-medium text-gray-700">Data</label>  
+            <div class="event-input-list"> 
+                <input id="datepicker1" name="datepicker1" datepicker datepicker-format="yyyy-mm-dd" value="{{ isset($formFields['datepicker1']) ? $formFields['datepicker1'] : '' }}" type="text" 
+                class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Selecionar Data">
+                <input id="datepicker2" name="datepicker2" datepicker datepicker-format="yyyy-mm-dd" value="{{ isset($formFields['datepicker2']) ? $formFields['datepicker2'] : '' }}" type="text" 
+                class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Selecionar Data">
+            </div>
         </div>
 
         
@@ -160,3 +168,4 @@
 
 <script src="https://cdn.jsdelivr.net/npm/flowbite@2.5.2/dist/flowbite.min.js"></script>
 @vite('resources/js/formListEventsManager.js')
+@vite('resources/js/hidder.js')
