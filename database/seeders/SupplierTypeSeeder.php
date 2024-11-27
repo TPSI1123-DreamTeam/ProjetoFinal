25094d3b9d13742c08478cf1db1a363eac873c1d
Gil Costa <gilberto.costa.t0127537@edu.atec.pt>
2024-11-27 21:10:17 +0000

    Error fix on suppliertype seeder

--- database/seeders/SupplierTypeSeeder.php
+++ database/seeders/SupplierTypeSeeder.php
@@ -4,7 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-use App\Models\Suppliertype;
+use App\Models\SupplierType;
 
 class SupplierTypeSeeder extends Seeder
 {
