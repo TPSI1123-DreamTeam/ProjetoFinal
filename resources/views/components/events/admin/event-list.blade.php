807a39c6a2d4fd7b9d433efecb7b6f1c5422102f
Gilberto Costa <158103790+GilCosta97@users.noreply.github.com>
2024-11-29 21:41:54 +0000

    Responsiveness to admin dashboard, event list & user list (#103)

--- resources/views/components/events/admin/event-list.blade.php
+++ resources/views/components/events/admin/event-list.blade.php
@@ -1,5 +1,3 @@
-<link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
- 
 <div class="event-wrapper">
     <h1>Lista de Eventos</h1>
  </div>
@@ -20,15 +18,17 @@
      <tbody>
          @foreach($events as $event)
          <tr>
-             <td>{{ $loop->iteration }}</td>
-             <td>{{ $event->name }}</td>
-             <td>{{ @count($event->users) }}</td>
-             <td>{{ $event->category->description }}</td>
-             <td>{{ date('Y-m-d', strtotime($event->start_date)) }}</td>
-             <td>{{ date('H:i', strtotime($event->start_time))}}</td>
-             <td>{{ $event->amount }}€</td>
-             <td>{{ $event->event_status }}</td>
+             <td data-cell="número">{{ $loop->iteration }}</td>
+             <td data-cell="nome">{{ $event->name }}</td>
+             <td data-cell="participantes">{{ @count($event->users) }}</td>
+             <td data-cell="descrição">{{ $event->category->description }}</td>
+             <td data-cell="data">{{ date('Y-m-d', strtotime($event->start_date)) }}</td>
+             <td data-cell="hora de inicio">{{ date('H:i', strtotime($event->start_time))}}</td>
+             <td data-cell="custo estimado">{{ $event->amount }}€</td>
+             <td data-cell="estado">{{ $event->event_status }}</td>
          @endforeach
      </tbody>
  </table>
-{{ $events->links() }}
+ <div class="pagination-user-list">
+    {{ $events->links() }}
+</div>
--- resources/views/components/users/users-list.blade.php
+++ resources/views/components/users/users-list.blade.php
@@ -1,68 +1,66 @@
-<link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
-    <h1>Gestão de Utilizadores</h1>
+<div class="table-wrapper">
+<h1 class="user-management">Gestão de Utilizadores</h1>
 
-    <div class="linha-divisoria"></div>
-
-    <ul role="list" class="list">
-
+<div class="linha-divisoria"></div>
+<table>
+    <thead>
+        <tr>
+            <th>Nome</th>
+            <th>Email</th>
+            <th>Função</th>
+            <th>Alterar Função</th>
+            <th>Alterar Password</th>
+        </tr>
+    </thead>
+    <tbody>
         @foreach ($users as $user)
-        <li class="card-user">
-            <!-- Informação do Utilizador -->
-            <div class="user-card-header">
-                <img src="{{ $user->image }}" alt="{{ $user->name }}">
-                <div>
-                    <p class="user-nameEmail">{{ $user->name }}</p>
-                    <p class="user-nameEmail">{{ $user->email }}</p>
-                    <p class="user-role-change">
-                        <span >Função:</span>
-                        @switch($user->role_id)
-                            @case(1) <span>Administrador</span> @break
-                            @case(2) <span>Gestor de Eventos</span> @break
-                            @case(3) <span>Proprietário de Eventos</span> @break
-                            @case(4) <span>Participante</span> @break
-                        @endswitch
-                    </p>
-                </div>
-            </div>
-
-            <!-- Ações -->
-            <div>
-                <!-- Alterar Função -->
+        <tr>
+            <!-- Nome -->
+            <td data-cell="nome">{{ $user->name }}</td>
+            <!-- Email -->
+            <td  data-cell="email">{{ $user->email }}</td>
+            <!-- Função -->
+            <td data-cell="função">
+                @switch($user->role_id)
+                    @case(1) Administrador @break
+                    @case(2) Gestor @break
+                    @case(3) Proprietário @break
+                    @case(4) Participante @break
+                @endswitch
+            </td>
+            <!-- Alterar Função -->
+            <td data-cell="alterar função">
+                @if($user->role_id != 1)
                 <form action="{{ route('users.update-role', $user->id) }}" method="POST">
                     @csrf
                     @method('PUT')
-                    @if($user->role_id != 1)
                     <select name="role_id">
-                        <option value="2" {{ $user->role_id == 2 ? 'selected' : '' }}>Gestor de Eventos</option>
-                        <option value="3" {{ $user->role_id == 3 ? 'selected' : '' }}>Proprietário de Evento</option>
+                        <option value="2" {{ $user->role_id == 2 ? 'selected' : '' }}>Gestor</option>
+                        <option value="3" {{ $user->role_id == 3 ? 'selected' : '' }}>Proprietário</option>
                         <option value="4" {{ $user->role_id == 4 ? 'selected' : '' }}>Participante</option>
                     </select>
                     <button type="submit">Atualizar</button>
-                    @endif
-
                 </form>
-
-                <!-- Redefinir Password -->
+                @endif
+            </td>
+            <!-- Redefinir Palavra-passe -->
+            <td data-cell="alterar password">
                 <form action="{{ route('users.reset-password', $user->id) }}" method="POST">
                     @csrf
                     @method('POST')
-                    <div class="mb-4">
-                        <label for="password" >New Password</label>
-                        <input
-                            id="password"
-                            name="password"
-                            type="password"
-                            required
-                            >
-                    </div>
+                        <input id="password" name="password" type="password" required>
                     <button type="submit">
-                        Alterar senha
+                        Alterar
                     </button>
                 </form>
-
-            </div>
-        </li>
+            </td>
+        </tr>
         @endforeach
-    </ul>
+    </tbody>
+</table>
+</div>
+
 <!-- Paginação -->
-{{ $users->links() }}
+<div class="pagination-user-list">
+    {{ $users->links() }}
+</div>
