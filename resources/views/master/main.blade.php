6cf74f4088beeafe220a7e5888d66743b5acb0fb
GilCosta <gilberto.costa.t0127537@edu.atec.pt>
2024-12-04 21:50:23 +0000

    iphone responsiveness test fix

--- resources/views/master/main.blade.php
+++ resources/views/master/main.blade.php
@@ -2,7 +2,7 @@
 <html lang="pt">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>@yield('title', 'Prime Time Events')</title>
     @vite('resources/css/client-side/app.css')
