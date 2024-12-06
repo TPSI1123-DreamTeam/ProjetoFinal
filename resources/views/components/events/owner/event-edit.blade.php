7ae5dd2ccbd6dddbe87de3f83bca04dd9cbd84a6
Eliezer Rangel <86741316+eliezerangel@users.noreply.github.com>
2024-12-06 19:23:36 +0000

    Added version one of event schedule (#141)

--- resources/views/components/events/owner/event-edit.blade.php
+++ resources/views/components/events/owner/event-edit.blade.php
@@ -5,102 +5,46 @@
     <form  method="POST" action="{{ url('/events/'.$event->id) }}" enctype="multipart/form-data" class="show-event-form">
         @method('PUT')
         @csrf
-        @if( $event->image )
         <div class="show-event-image">
             <img src="/images/{{ $event->image }}" alt="Imagem do Evento">
         </div>
         <div class="custom-file">              
-                <label for="image">Imagem:</label>
-                <input  type="file" id="image" name="image" class="form-control @error('image') is-invalid @enderror"> 
-                @error('image')
-                    <div class="alert alert-danger">{{ $message }}</div>
-                @enderror               
-            </div> 
-        @else    
-            <img src="/images/noimage_default.jpg" alt="...">
-            <div class="custom-file">              
-                <label for="image">Imagem:</label>
-                <input  type="file" id="image" name="image" class="form-control @error('image') is-invalid @enderror"> 
-                @error('image')
-                    <div class="alert alert-danger">{{ $message }}</div>
-                @enderror               
-            </div> 
-        @endif 
-
+            <label for="image">Imagem:</label>
+            <input  type="file" id="image" name="image" class="form-control @error('image') is-invalid @enderror"> 
+            @error('image')
+                <div class="alert alert-danger">{{ $message }}</div>
+            @enderror               
+        </div>
         <div class="grid-form">
             <div class="event-name">
-                <label for="inputEmail4">Nome do Evento</label> 
-                <input type="text" name="name" id="name" value="{{ $event->name }}" placeholder="Nome do Evento" class="form-control @error('localization') is-invalid @enderror"  >
-                @error('name')
-                    <div class="alert alert-danger">{{ $message }}</div>
-                @enderror
-            </div>   
+                <label for="name">Nome do Evento</label>
+                <input type="text"name="name" id="name" value="{{ $event->name }}"  class="form-control">
+            </div>
             <div class="event-local">
-                <label for="inputlocalization">Localização</label>
-                <input type="text" class="form-control @error('localization') is-invalid @enderror" name="localization"  id="localization" value="{{ $event->localization }}" placeholder="Localização do evento"  >
-                @error('localization')
-                    <div class="alert alert-danger">{{ $message }}</div>
-                @enderror
+                <label for="localization">Localização</label>
+                <input type="text" class="form-control" name="localization"  id="localization" value="{{ $event->localization }}">
             </div>
             <div class="event-participants">
-                <label for="number_of_participants">Nº de Participantes</label>
-                <input type="text" class="form-control @error('number_of_participants') is-invalid @enderror" name="number_of_participants"  id="number_of_participants" value="{{ $event->number_of_participants  }}" oninput="this.value = this.value.replace(/[^0-9]/g, '');"  >
-                @error('number_of_participants')
-                    <div class="alert alert-danger">{{ $message }}</div>
-                @enderror
+                <label for="number_of_participants">Participantes</label>
+                <input type="text" class="form-control" name="number_of_participants"  id="number_of_participants" 
+                value="{{ $event->number_of_participants }}" min="30" max="100000" 
+                oninput="this.value = this.value.replace(/[^0-9.]/g, '').replace(/(\..*?)\..*/g, '$1');">
             </div>
             <div class="event-description">
-                <label for="inputAddress">Descrição do Evento</label>
-                <input type="text-area" class="form-control @error('description') is-invalid @enderror" name="description"  id="description" placeholder="Descrição do evento" value="{{ $event->description }}"  >
-                @error('description')
-                    <div class="alert alert-danger">{{ $message }}</div>
-                @enderror
+                <label for="description">Descrição do Evento</label>
+                <input type="text-area" class="form-control" name="description"  id="description"  value="{{ $event->description }}">
             </div>
-            <div class="event-category">               
-                <label class="" for="inputGroupSelect01">Categoria</label>
-                <select id="type" name="category_id" id="category_id" class="form-control @error('category_id') is-invalid @enderror"  >
-                    <option disabled>Selecione a categoria:</option>
+            <div class="event-category">
+                <label for="category_id">Categoria</label>
+                <select id="category_id" name="category_id"  class="form-control">
+                    <option  >Selecione a categoria:</option>
                     @foreach ($categories as $item)
-                        <option value="{{ $item->id }}" @if( $item->id == $event->category_id) selected @endif >{{ $item->description }}</option>
-                    @endforeach
+                    <option value="{{ $item->id }}" @if( $item->id == $event->category_id) selected @endif >{{ $item->description }}</option>
+                        @endforeach
                 </select>  
-                @error('category_id')
-                    <div class="alert alert-danger">{{ $message }}</div>
-                @enderror     
-            </div>        
-            <div class="start_date">
-                <label for="inputAddress2">Data Inicio do Evento</label>
-                <input type="date" class="form-control @error('start_date') is-invalid @enderror" name="start_date" id="start_date" value="{{ $event->start_date  }}" min="{{ now()->toDateString() }}"   >
-                @error('start_date')
-                    <div class="alert alert-danger">{{ $message }}</div>
-                @enderror
-            </div>
-            <div class="start_time">
-                <label for="inputAddress2">Hora Inicio do Evento</label>
-                <input type="time" class="form-control @error('start_time') is-invalid @enderror" name="start_time" id="start_time" 
-                value="{{ date('H:i', strtotime($event->start_time)) }}"    pattern="^([01][0-9]|2[0-3]):[0-5][0-9]$"  placeholder="HH:MM" >
-                @error('start_time')
-                    <div class="alert alert-danger">{{ $message }}</div>
-                @enderror
-            </div>
-            <div class="end_date">
-                <label for="inputAddress2">Data Fim do Evento</label>
-                <input type="date" class="form-control @error('end_date') is-invalid @enderror" name="end_date" id="end_date" value="{{ $event->end_date  }}"  min="{{ now()->toDateString() }}"   >
-                @error('end_date')
-                    <div class="alert alert-danger">{{ $message }}</div>
-                @enderror
-            </div>
-            <div class="end_time">
-                <label for="inputAddress2">Hora Fim do Evento</label>
-                <input type="time" class="form-control @error('end_time') is-invalid @enderror" name="end_time" id="end_time" value="{{ date('H:i', strtotime($event->end_time)) }}"
-                pattern="^([01][0-9]|2[0-3]):[0-5][0-9]$" 
-                placeholder="HH:MM" >
-                @error('end_time')
-                    <div class="alert alert-danger">{{ $message }}</div>
-                @enderror
             </div>
             <div class="event-type">
-                <label for="inputState">Tipo de Evento</label>
+            <label for="inputState">Tipo de Evento</label>
                 <select id="type" name="type"  class="form-control @error('type') is-invalid @enderror"  >
                     <option disabled>Escolher o tipo de evento...</option>
                     <option value="Publico" @if($event->type == "Publico") selected @endif >Público</option>
@@ -109,28 +53,63 @@
                 @error('type')
                     <div class="alert alert-danger">{{ $message }}</div>
                 @enderror
-            </div>  
+            </div>
+            <div>
+                <label for="start_date">Data do Evento</label>
+                <input type="date" class="form-control" name="start_date" id="start_date" value="{{ $event->start_date }}">
+            </div>
+            <div>
+                <label for="start_time">Hora</label>
+                <input type="time" class="form-control" name="start_time" id="start_time"  value="{{ date('H:i', strtotime($event->start_time)) }}"  pattern="^([01][0-9]|2[0-3]):[0-5][0-9]$"  placeholder="HH:MM">
+            </div>
+            <div>
+                <label for="end_date">Data do Fim do Evento</label>
+                <input type="date" class="form-control" name="end_date" id="end_date" value="{{ $event->end_date }}">
+            </div>
+            <div>
+                <label for="end_time">Hora</label>
+                <input type="time" class="form-control" name="end_time" id="end_time"  value="{{ date('H:i', strtotime($event->end_time)) }}" pattern="^([01][0-9]|2[0-3]):[0-5][0-9]$"   placeholder="HH:MM">
+            </div>
+            <div>
+                <label for="number_of_participants">Custo do Evento</label>
+                <input type="text" class="form-control" name="amount"  id="amount" value="{{ $event->amount }}" 
+                min="30" max="1000000" oninput="this.value = this.value.replace(/[^0-9.]/g, '').replace(/(\..*?)\..*/g, '$1');" 
+                step="0.01" min="0" >
+            </div>
+            <div>
+                <label for="ticket_amount">Custo do Bihete (Quando Aplicável)</label>
+                <input type="text" class="form-control" name="ticket_amount"  id="ticket_amount" value="{{ $event->ticket_amount }}" 
+                min="30" max="1000000" oninput="this.value = this.value.replace(/[^0-9.]/g, '').replace(/(\..*?)\..*/g, '$1');" 
+                step="0.01" min="0" >
+            </div>
         </div>
-            <div class="checkbox-group">
-                @php
-                    $servicesArray = json_decode($event->services_default_array, true);
-                @endphp
+        <div class="edit-supplier-btn">
+            <a href="{{ url('/schedules/'.$event->id) }}" class="update-supplier-btn">Gerir Agenda do Evento</a> 
+        </div>
+        <div class="checkbox-group">
+            @php
+                $servicesArray = json_decode($event->services_default_array, true);
+            @endphp
 
-                @foreach($SupplierType as $type)
-                <div class="suppliers-input">
-                    <input type="checkbox" id="suppliers[]" name="suppliers[]" value="{{ $type->id }}" 
-                        @if(!empty($servicesArray) && in_array($type->id, $servicesArray))
-                            checked
-                        @endif>
-                        <label for="suppliers[]">{{ $type->name }}</label>
-                </div>
-                @endforeach
+            @foreach($SupplierType as $type)
+            <div class="suppliers-input">
+                <input type="checkbox" id="suppliers[]" name="suppliers[]" value="{{ $type->id }}" 
+                    @if(!empty($servicesArray) && in_array($type->id, $servicesArray))
+                        checked
+                    @endif>
+                    <label for="suppliers[]">{{ $type->name }}</label>
+            </div>
+            @endforeach
         </div>
-    <div class="edit-event-back-btn">
-        <a href="/events/owner" class="go-back-btn">Voltar à lista de eventos</a>
-        <button type="submit" class="update-event-btn">Atualizar Evento</button>  
-    </div>
+        <div class="edit-event-back-btn">
+            <a href="/events/owner" class="go-back-btn">Voltar à lista de eventos</a>
+            <button type="submit" class="update-event-btn">Atualizar Evento</button>  
+        </div>
     </form>
 </div>
 
-@vite('resources/js/form-create-event.js')
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.3/dist/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+
+@vite('resources/js/form-create-event.js') 
