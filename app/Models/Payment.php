7995de84fc6ff5277b195cb4f2c2d5e3ebe1e7bc
Vsgroundnet <33607555+Vsgroundnet@users.noreply.github.com>
2024-12-11 22:55:56 +0000

    Removed code that was commented in a few files (#195)

--- app/Models/Payment.php
+++ app/Models/Payment.php
@@ -19,21 +19,11 @@ class Payment extends Model
         'status' => 'boolean',
     ];
 
-    // public function user()
-    // {
-    //     return $this->belongsTo(User::class);
-    // }
-
-    // public function event()
-    // {
-    //     return $this->belongsTo(Event::class);
-    // }
-
     public function current_accounts(): BelongsToMany{
 
         return $this->belongsToMany(CurrentAccount::class);
     }
 
 
-    
+
 }
