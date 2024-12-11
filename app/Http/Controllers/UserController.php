168cd9b2e6919c16ae71e8bdd8d435538c17ccaf
Pedro Nuno Sá Ferreira <150871417+pedroferr94@users.noreply.github.com>
2024-12-11 11:15:45 +0000

    added search filters to users and events on admin dashboard (#187)
    * added search filters to users on admin dashboard
    
    * added search filter to admin event list
    
    ---------
    
    Co-authored-by: Pedro Sá Ferreira <pedro.saferreira1994@gmail.com>

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -10,9 +10,27 @@ use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
-        $users = User::orderBy('id')->paginate(5);
+        // Inicia a query base
+        $query = User::query();
+
+        // Verifica se há termo de pesquisa
+        if ($request->filled('search')) {
+            $search = $request->input('search');
+            $query->where('name', 'like', '%' . $search . '%')
+                  ->orWhere('email', 'like', '%' . $search . '%');
+        }
+
+        if ($request->filled('role_id')) {
+            $roleId = $request->input('role_id');
+            $query->where('role_id', $roleId);
+        }
+
+        // Ordena e pagina os resultados
+        $users = $query->orderBy('id')->paginate(5);
+
+        // Retorna para a view
         return view('pages.users.index', ['users' => $users]);
     }
 
@@ -41,6 +59,14 @@ class UserController extends Controller
         $user->save();
 
         return redirect()->back()->with('status', 'Password atualizada com sucesso!')->with('class', 'alert-success');
+    }
+
+    public function searchUser(Request $request)
+    {
+        $search = $request->input('search');
+        $users = User::where('name', 'like', '%' . $search . '%')->paginate(10);
+        $users = User::where('email', 'like', '%' . $search . '%')->paginate(10);
+        return view('pages.users.index', ['users' => $users]);
     }
 
 }
